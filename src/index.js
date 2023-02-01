e7f148c55714f2d5882da3019fa6e1364f1a69cf
corradoballabio <corradoballabio11@gmail.com>
2023-02-01 15:43:04 +0100

    Save the winning combination

--- src/index.js
+++ src/index.js
@@ -58,7 +58,7 @@ class Game extends React.Component {
 		const current = history[history.length - 1]
 		const squares = current.squares.slice()
 
-		if(calculateWinner(squares) || squares[i]) return
+		if(calculateWinner(squares).player || squares[i]) return
 
 		squares[i] = this.state.xIsNext ? 'X' : 'O'
 
@@ -106,7 +106,7 @@ class Game extends React.Component {
 			)
 		}).sort(() => this.state.isSortedAsc ? -1 : 1)
 
-		let status = winner ? 'Winner: ' + winner : 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+		let status = winner.player ? 'Winner: ' + winner.player : 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 		let sortOrder = this.state.isSortedAsc ? 'Descending' : 'Ascending'
 
 		const sorting = <button onClick={() => this.setSortingOrder()}>Sort {sortOrder}</button>
@@ -145,15 +145,20 @@ function calculateWinner(squares) {
 		[0, 4, 8],
 		[2, 4, 6],
 	];
+	let winner = {
+		player: null,
+		combo: []
+	}
 
 	for(let i = 0; i < lines.length; i++) {
 		const [a, b, c] = lines[i]
 
 		if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-			return squares[a]
+			winner.player = squares[a]
+			winner.combo= [a, b, c]
 		}
 	}
-	return null
+	return winner
 }
 
 function getGridPosition(i) {
